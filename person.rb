2031a4066e97f0d5b600a2b0949ab71600d3b42c
Thermiee <navyblack16@yahoo.com>
2022-05-11 12:29:44 +0100

    remove super declaration in person

--- person.rb
+++ person.rb
@@ -1,14 +1,12 @@
 require_relative './decorator'
 
 class Person < Nameable
-  super class Nameable
-          def initialize(age, name = 'Unknown', parent_permission: true)
-            @id = Random.rand(1..1000)
-            @name = name
-            @age = age
-            @parent_permission = parent_permission
-          end
-        end
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
   attr_accessor :name, :age
   attr_reader :id
