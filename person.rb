4c7d704c070f7c0340fd6b9e1c57f274d06f7d76
Thermiee <navyblack16@yahoo.com>
2022-05-12 13:47:45 +0100

    modify person.rb file

--- person.rb
+++ person.rb
@@ -1,16 +1,17 @@
 require_relative './decorator'
 
 class Person < Nameable
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rental = []
     super()
   end
-
-  attr_accessor :name, :age
-  attr_reader :id
 
   def can_use_services?
     if is_of_age? || @parent_permission
