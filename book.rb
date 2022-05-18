8c22ccd64517a811c28e32dbae4ed26a4fa4abe5
Dagi <dagi.zewdu.dz@gmail.com>
2022-05-18 17:52:11 +0300

    Added file integration

--- book.rb
+++ book.rb
@@ -1,7 +1,8 @@
 class Book
   attr_accessor :title, :author, :rentals
-
-  def initialize(title, author)
+   attr_reader :id
+  def initialize(title, author, id: nil)
+    @id=id || Random.rand(1..1000)
     @title = title
     @author = author
     @rentals = []
@@ -9,5 +10,17 @@ class Book
 
   def add_rental(date, person)
     @rentals << Rental.new(date, self, person)
+  end
+  def to_hash
+    hash = {}
+    hash[:class] = self.class
+    hash[:title] = @title
+    hash[:author] = @author
+    hash[:id] = @id
+    hash
+  end
+
+  def self.create_from_hash(hash)
+    Book.new(hash['title'], hash['author'], id: hash['id'])
   end
 end
--- student.rb
+++ student.rb
@@ -2,9 +2,8 @@ require_relative './person'
 
 class Student < Person
   attr_reader :classroom
-
-  def initialize(age, classroom, name, parent_permission:true)
-    super(age, name, parent_permission:parent_permission)
+  def initialize(age, classroom, name, parent_permission:true,id: nil)
+    super(age, name, parent_permission:parent_permission,id:id)
     @classroom = classroom
   end
 
@@ -14,5 +13,18 @@ class Student < Person
 
   def classroom=(classroom)
     @classroom = classroom classroom.students.push(self) unless classroom.students.include?(self)
+  end
+  def to_hash
+    hash = {}
+    hash[:class] = self.class
+    hash[:age] = @age
+    hash[:name] = @name
+    hash[:parent_permission] = @parent_permission
+    hash[:id] = @id
+    hash
+  end
+
+  def self.create_from_hash(hash)
+    Student.new(hash['age'],nil, hash['name'], parent_permission: hash['parent_permission'], id: hash['id'])
   end
 end
